f158a7f27370b1069e5458be469c254dd6eb4f28
Abde <abdenah17@gmail.com>
2025-12-24 18:38:34 +0100

    some base

--- parser.c
+++ parser.c
@@ -1,5 +1,19 @@
+#include "push_swap.h"
+
 int main(int ac, char **av)
 {
+    int i;
 
+    if (ac <= 2)
+        return (0);
+
+    i = 0;
+    while (i < ac)
+    {
+        t_list *node = malloc(sizeof(t_list));
+        if (!node)
+            return (0);
+        
+    }
 }
 
--- push_swap.h
+++ push_swap.h
@@ -1,6 +1,13 @@
 #ifndef PUSH_SWAP_H
 #define PUSH_SWAP_H
 
+typedef struct s_list{
+    int num;
+    int index;
+    struct s_list *next;
+    struct s_list *prv;
+}   t_list;
+
 #include <unistd.h>
 #include <stdlib.h>
 
