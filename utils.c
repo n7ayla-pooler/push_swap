7de59bf246458d1d9532bbdf4734e7556d43e563
Abdellatif Nahal <abdnahal@e1r8p6.1337.ma>
2026-01-07 14:33:54 +0100

    indexing

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: abdnahal <abdnahal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 14:17:57 by abdnahal          #+#    #+#             */
-/*   Updated: 2026/01/05 14:44:38 by abdnahal         ###   ########.fr       */
+/*   Updated: 2026/01/07 13:55:59 by abdnahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,26 @@ void sort_5(t_list **stack_a, t_list **stack_b)
     push(stack_a, stack_b, 'a');
     sort_3(stack_a);
     push(stack_a, stack_b, 'a');
+}
+
+void indexing(t_list *stack)
+{
+    int count;
+    t_list *curr;
+    t_list *head;
+    
+    count = 0;
+    head = stack;
+    while (stack)
+    {
+        stack->index = 0;
+        curr = head;
+        while (curr)
+        {
+            if (stack->num > curr->num)
+                stack->index += 1;
+            curr = curr->next;
+        }
+        stack = stack->next;
+    }
 }
